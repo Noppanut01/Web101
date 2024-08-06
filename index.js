01b08066dcce4cb0cc7e3afdbb3347a965c8d9f8
Noppanut01 <noppanut.jang2547@gmail.com>
2024-08-06 22:26:20 +0700

    Add validation

--- index.js
+++ index.js
@@ -1,27 +1,87 @@
-let submitData = () => {
+const validateData = (userData) => {
+    let error = []
+    if (!userData.firstname) {
+        error.push('กรุณาใส่ชื่อจริง')
+    }
+    if (!userData.firstname) {
+        error.push('กรุณาใส่ชื่อจริง')
+    }
+    if (!userData.firstname) {
+        error.push('กรุณาใส่ชื่อจริง')
+    }
+    if (!userData.lastname) {
+        error.push('กรุณาใส่นามสกุล')
+    }
+    if (!userData.age) {
+        error.push('กรุณาใส่อายุ')
+    }
+    if (!userData.gender) {
+        error.push('กรุณาใส่เพศ')
+    }
+    if (!userData.interests) {
+        error.push('กรุณาใส่ความสนใจ')
+    }
+    if (!userData.description) {
+        error.push('กรุณาใส่ลายละเอียดของคุณ')
+    }
+    return error
+}
+
+let submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname')
     let lastNameDOM = document.querySelector('input[name=lastname')
     let ageDOM = document.querySelector('input[name=age]')
-    let genderDOM = document.querySelector('input[name=gender]:checked')
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked')
+    let genderDOM = document.querySelector('input[name=gender]:checked') || {}
+    let interestDOMs = document.querySelectorAll('input[name=interest]:checked') || {}
     let descriptionDOM = document.querySelector('textarea[name=description]')
+    let messageDOM = document.getElementById('message')
 
-    let interest = ''
-
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
-        if (i < interestDOMs.length - 1) {
-            interest += ', '
+    try {
+        let interest = ''
+        for (let i = 0; i < interestDOMs.length; i++) {
+            interest += interestDOMs[i].value
+            if (i < interestDOMs.length - 1) {
+                interest += ', '
+            }
         }
+        let userData = {
+            firstname: firstNameDOM.value,
+            lastname: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            interests: interest,
+            description: descriptionDOM.value
+        }
+
+        console.log('submit data', userData)
+        const errors = validateData(userData)
+
+        if (errors.length > 0) {
+            throw {
+                message: 'กรอกข้อมูลไม่ครบ',
+                errors: errors,
+            }
+        }
+        const response = await axios.post('http://localhost:8000/users', userData)
+        console.log('response', response.data)
+        messageDOM.innerText = 'บันทึกข้อมูลเรียบร้อย'
+        messageDOM.className = 'message success'
+    } catch (error) {
+        console.log('error message', error.message)
+        console.log('error', error.errors)
+        // if (error.response) {
+        //     console.log(error.response.data.message)
+        // }
+        let htmlData = '<div>'
+        htmlData += `<div>${error.message}</div>`
+        htmlData += '<ul>'
+        for (let i = 0; i < error.errors.length; i++) {
+            htmlData += `<li>${error.errors[i]}</li>`
+        }
+        htmlData += '</ul>'
+        htmlData += '</div>'
+        messageDOM.innerHTML = htmlData
+        messageDOM.className = 'message danger'
     }
-    let userData = {
-        firstname: firstNameDOM.value,
-        lastname: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        interest: interest,
-        description: descriptionDOM.value
-    }
-    console.log(userData)
 
 }
